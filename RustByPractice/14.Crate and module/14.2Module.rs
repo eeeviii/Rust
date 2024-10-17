1d3175dd9bf512adfdd05d6a8b53642b645edcc5
eviii <danyatkachuk2005@gmail.com>
2024-10-17 22:10:49 +0300

    Перемістив папки в RustByPractice

---
+++ RustByPractice/14.Crate and module/14.2Module.rs
@@ -0,0 +1,98 @@
+//1.
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+
+        fn complain() {} 
+    }
+}
+
+//2.
+pub mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        pub fn seat_at_table() {}
+    }
+
+    pub mod serving {
+        pub fn take_order() {}
+
+        pub fn serve_order() {}
+
+        pub fn take_payment() {}
+
+        fn complain() {} 
+    }
+}
+
+pub fn eat_at_restaurant() {
+    
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    front_of_house::hosting::add_to_waitlist();
+}
+
+//3.
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::front_of_house::serving::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+//4.
+mod front_of_house;
+mod back_of_house;
+pub fn eat_at_restaurant() -> String {
+    front_of_house::hosting::add_to_waitlist();
+
+    back_of_house::cook_order();
+
+    String::from("yummy yummy!")
+}
+
+use crate::front_of_house;
+pub fn fix_incorrect_order() {
+    cook_order();
+    front_of_house::serving::serve_order();
+}
+
+pub fn cook_order() {}
+
+pub mod hosting;
+pub mod serving;
+
+pub fn add_to_waitlist() {}
+
+pub fn seat_at_table() -> String {
+    String::from("sit down please")
+}
+
+pub fn take_order() {}
+
+pub fn serve_order() {}
+
+pub fn take_payment() {}
+
+fn complain() {} 
+
+//5.
+mod front_of_house;
+
+fn main() {
+    assert_eq!(front_of_house::hosting::seat_at_table(), "sit down please");
+    assert_eq!(hello_package::eat_at_restaurant(),"yummy yummy!");
+}
