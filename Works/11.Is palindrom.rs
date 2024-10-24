55b6a0e3fec0c72fa3ac0edbbafd91f5d4e1f5c5
eeeviii <danyatkachuk2005@gmail.com>
2024-10-24 22:57:04 +0300

    Update 11.Is palindrom.rs

--- Works/11.Is palindrom.rs
+++ Works/11.Is palindrom.rs
@@ -1,18 +1,28 @@
 fn is_palindrome(x: u32) -> bool {
-    let original = x.to_string();                // Перетворюємо число на рядок
-    let reversed: String = original.chars().rev().collect();  // Реверсуємо рядок
-    original == reversed                         // Порівнюємо оригінальний та реверсований рядки
+    let original = x.to_string();
+    let reversed: String = original.chars().rev().collect();
+    original == reversed
 }
 
-#[test]
-fn test() {
-    let data = [
-        (123, false),
-        (121, true),
-        (1221, true),
-    ];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    data.iter().for_each(|(n, exp)| {
-        assert_eq!(is_palindrome(*n), *exp);
-    });
+    #[test]
+    fn test_is_palindrome() {
+        let data = [
+            (123, false),
+            (121, true),
+            (1221, true),
+        ];
+
+        data.iter().for_each(|(n, exp)| {
+            assert_eq!(is_palindrome(*n), *exp);
+        });
+    }
+}
+
+fn main() {
+    let num = 121;
+    println!("Is {} a palindrome? {}", num, is_palindrome(num));
 }
